5496649d8a1c8257886534f7fde733dd2b9513bd
linder0209 <linder0209@126.com>
2014-03-17 23:13:29 +0800

    利用yo构建angular项目

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('hopefutureBlogApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
