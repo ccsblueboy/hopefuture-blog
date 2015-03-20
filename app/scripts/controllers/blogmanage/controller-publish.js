3fea65aafbbd0ada1da4bf72c26fc31233418e8a
linder0209 <linder0209@126.com>
2015-03-20 18:32:30 +0800

    data and code

--- app/scripts/controllers/blogmanage/controller-publish.js
+++ app/scripts/controllers/blogmanage/controller-publish.js
@@ -34,7 +34,8 @@ angular.module('hopefutureBlogApp').controller('PublishCtrl', function ($scope, 
     articleLink: '',//文章永久链接，取相对地址
     type: 'richText',//文章类型
     categories: [],//文章所属分类
-    labels: []//文章标签
+    labels: [],//文章标签
+	catalogue: 0//是否生成目录
   };
 
   /**
